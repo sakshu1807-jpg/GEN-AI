6d693396c4f12da77b4725cbcb5aa1707838fed6
Saksham Manchanda <sakshu1807@gmail.com>
2026-07-29 22:43:59 +0530

    Modified the tool for grounding

--- Generative_AI/Agents/Agents_projects/multi_agent.py
+++ Generative_AI/Agents/Agents_projects/multi_agent.py
@@ -1,8 +1,10 @@
+from tools import get_source_data, user_approval
+
 # Langchain Components
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, SystemMessage
 
 # Agents Components
 from langchain.agents import create_agent
@@ -11,9 +13,46 @@ from langchain.agents.middleware import(
     SummarizationMiddleware
 )
 from langgraph.checkpoint.memory import InMemorySaver
+from pydantic import BaseModel, AnyHttpUrl
+from typing import List
 
 # Environmental Variables
 from dotenv import load_dotenv
 
 load_dotenv()
 
+class Response(BaseModel):
+
+    summary_content: str
+    urls: List[AnyHttpUrl]
+
+small_model = ChatMistralAI(
+    model_name= 'mistral-small-latest'
+)
+
+medium_model = ChatMistralAI(
+
+    model_name= 'mistral-medium-latest'
+)
+
+system_msg_1 = SystemMessage(
+    content= """
+    A helpful AI Assisstant, who recieves a raw data of the sources from the tool. Your goal is to fetch all 
+    the urls present in the output of the tool and create a batch of those urls.
+    Generate your response as :- 
+    1. A summary of all the content given to you.
+    2. A batch of all the urls.
+    """
+)
+
+
+agent_1 = create_agent(
+    model= medium_model,
+    tools= [get_source_data],
+    system_prompt= system_msg_1,
+    checkpointer= InMemorySaver(),
+    middleware= [user_approval]
+)
+
+you = input("You :- ")
+urls = agent_1.invoke(you)
--- Generative_AI/Agents/Agents_projects/tools.py
+++ Generative_AI/Agents/Agents_projects/tools.py
@@ -3,34 +3,71 @@ import requests
 from langchain.tools import tool
 from langchain.agents.middleware import wrap_tool_call
 from tavily import TavilyClient
+from langchain_core.messages import ToolMessage
+
+# LLm for grounding the user query
+from langchain_mistralai import ChatMistralAI
 
 # Tool - 1 : Extracts the current data along with the URLs
 
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 
 @tool
-def get_source_data(query: str):
-    """This tool is used to extract all the data from different sources as per the query given by the user
-    ans returns all the urls of the source it visited or searched."""
+def get_source_data(user_field: str):
+    """This tool is used to extract all the data from different sources as per the field of study given by the user
+    ans returns all the content along with the urls of the source it visited or searched."""
+
+    llm = ChatMistralAI(
+        model_name= 'mistral-small-latest',
+        api_key= MISTRAL_API_KEY
+    )
+    
+    career_websites = [
+        "onetonline.org",
+        "careeronestop.org",
+        "mindler.com",
+        "sarvgyan.com",
+        "edumilestones.com",
+        "collegeboard.org",
+        "mba.com",
+        "ets.org",
+        "glassdoor.com",
+        "linkedin.com"
+    ]
 
     client = TavilyClient(
         api_key= TAVILY_API_KEY,
     )
 
+    final_query = f"""Inside the included professional career databases, educational planning portals,
+                    and competitive exam boards, analyze the field of {user_field}.
+                    Extract the definitive career paths, specific entrance or standardized exams required,
+                    and the future significance/job growth outlook for this field."""
+    
     raw_data = client.search(
-        query= query,
+        query= final_query,
         search_depth= "advanced",
-        max_results= 10
+        max_results= 10,
+        include_domains= career_websites
     )
 
-    urls = []
+    return raw_data
 
-    for result in raw_data['results']:
-        url = result['url']
+@wrap_tool_call
+def user_approval(request, handler):
+    """This custom middleware seeks the approval from the user to whether geenrate the final report or not."""
 
-        urls.append(url)
+    print(" ✅ Data from Different Sources Collected.\n")
 
-    return urls
+    user_input = input("Enter YES to generate the final report")
+
+    if user_input.strip().lower() != 'yes':
+        return ToolMessage(
+            content= "User denied to generate the final report."
+        )
+    
+    return handler(request)
 
 
 
